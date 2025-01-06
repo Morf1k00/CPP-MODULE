e8537cbe5b37c96af6d9e19fa176cfae84578f9e
rostik <rostik@DESKTOP-82C22FI.localdomain>
2025-01-06 13:13:54 +0100

    allmost done

--- CPP01/ex00/src/Zombie.cpp
+++ CPP01/ex00/src/Zombie.cpp
@@ -10,3 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/Zombie.hpp"
+
+Zombie::Zombie(std::string name){
+    this->_name = name;
+}
+
+Zombie::~Zombie(){
+        std::cout << "Zombie " << _name << " is destroyed." << std::endl;
+}
+
+void Zombie::announce() const{
+    std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
