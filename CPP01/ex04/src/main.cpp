131d4eb6ce7fccfb6fc2e469b3ad61ad678ad5b7
Morf1k00 <rostik0990679212@gmail.com>
2025-01-23 13:46:28 +0100

    fix my program

--- CPP01/ex04/src/main.cpp
+++ CPP01/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rostik <rostik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 17:45:43 by rostik            #+#    #+#             */
-/*   Updated: 2025/01/22 17:46:30 by rostik           ###   ########.fr       */
+/*   Updated: 2025/01/23 13:44:16 by rostik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,24 +39,21 @@ int check_file(int argc, char **argv, std::ifstream &file){
 int main(int arc, char **argv){
 
     int i = 0;
-    std::string s1;
-    std::string s2;
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
     std::string line;
     std::ifstream fileName;
     std::ofstream newFile;
     
     if(check_file(arc, argv, fileName) == 0)
         return 0;
-    
-    s1 = argv[2];
-    s2 = argv[3];
 
     newFile.open((std::string(argv[1]) + ".replace").c_str());
     while(getline(fileName, line))
     {
         i = 0;
         while(line.find(s1) != std::string::npos){
-            i = line.find(s1, i);
+            i = line.find(s1, i); // fix problem(before was problem with index from which i should start changing arguments)
             line.erase(i, s1.length());
             line.insert(i, s2);
             i+= s2.length();
